ddb38e21a5370985e22a0d8a119129f64e9212e5
Farida Elhusseiny <faridaelhussieny@gmail.com>
2023-08-11 13:57:51 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - source code
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char alphabet;
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchhar('\n');
+	return (0);
+}
